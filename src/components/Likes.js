a15edb9bd83c7e77e412c99bc83e334c75c280dc
Brandi Burnell <brandiburnell6@gmail.com>
2023-12-09 18:44:48 -0700

    complete functionality

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Likes({upvotes, downvotes}) {
+    const [likes, setLikes] = useState(upvotes);
+    const [dislikes, setDislikes] = useState(downvotes);
+
+    function handleClick(e) {
+        if (e.target.id === "like-btn") {
+            setLikes(() => likes + 1);
+        }
+        else {
+            setDislikes(() => dislikes + 1);
+        }
+    }
 
     return (
         <div>
-            <button>{upvotes} 👍</button>
-            <button>{downvotes} 👎</button>
+            <button onClick={handleClick} id="like-btn">{likes} 👍</button>
+            <button onClick={handleClick} id="dislike-btn">{dislikes} 👎</button>
         </div>
     );
 }
--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Views from "./Views";
 import Likes from "./Likes";
 import Comments from "./Comments";
 
 function Stats({video}) {
+    const [showComments, setShowComments] = useState(true);
+
     console.log(video);
+
+    function handleClick() {
+        setShowComments(!showComments);
+    }
+
     return (
         <div>
             <h2>React Today and Tomorrow and 90% Cleaner React With Hooks</h2>
             <Views views={video.views} />
             <Likes upvotes={video.upvotes} downvotes={video.downvotes} />
             <br></br>
-            <button>Hide Comments</button>
+            <button onClick={handleClick}>Hide Comments</button>
             <hr></hr>
-            <Comments comments={video.comments}/>
+            { showComments ? <Comments comments={video.comments}/> : null }
         </div>
     );
 }
